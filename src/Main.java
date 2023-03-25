4c3d1939406a7a5a6bb9ee7288945ff3821435b5
Borkata02 <bgeorgiev02@gmail.com>
2023-03-25 22:05:36 +0200

    Testing first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a;
+        int a,b;
         a=5;
+        b=4;
         System.out.println(a);
+        System.out.println(b);
     }
 }
